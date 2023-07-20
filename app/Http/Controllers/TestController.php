3a2eb356e5000c805d0c30f734a7e48634230609
sato.yasutaka <sato.yasutaka@i3design.co.jp>
2023-07-20 12:08:41 +0900

    Laravel Breeze

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Test;
+use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
     public function index()
     {
-        return view('tests.test');
+        //Eloquent
+        $values = Test::all();
+        $count = Test::count();
+        $first = Test::findOrFail(1);
+        $whereAAA = Test::where('text','=','aaa')->get();
+
+        //クエリビルダ - 速度的には速いが、Eloquentの方がメリットが多い
+        $queryBuilder = DB::table('tests')->where('text','=','aaa')
+        ->select('id','text')
+        ->get();
+        // ->first();
+
+        dd($values, $count, $first, $whereAAA, $queryBuilder);
+
+        // dd($values);
+        return view('tests.test',compact('values'));
     }
 }
